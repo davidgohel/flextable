189653e5528daf2e0a1161a384227c508d392e05
dgohel <david.gohel@ardata.fr>
2016-07-01 18:23:08 +0200

    testthat

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(flextable)
+
+test_check("flextable")
---
+++ tests/testthat/test-pptx-tables.R
@@ -0,0 +1,25 @@
+context("ppt table structure")
+
+library(utils)
+library(xml2)
+
+test_that("row height is valid", {
+  ft <- flextable( head( iris) )
+  pptx_file <- write_pptx("test.pptx", ft)
+
+  main_folder <- file.path(getwd(), "pptx_folder" )
+  unzip(pptx_file, exdir = main_folder)
+
+  slide_file <- file.path(main_folder, "/ppt/slides/slide1.xml" )
+  doc <- read_xml( slide_file )
+
+  nodes <- xml_find_all(doc, "//p:graphicFrame/a:graphic/a:graphicData/a:tbl/a:tr")
+  h_values <- map_chr( nodes, xml_attr, attr="h")
+  h_values <- as.integer(h_values)
+  expect_true( all( is.finite( h_values ) ) )
+  expect_true( all( h_values > 0 ) )
+
+  unlink(main_folder, recursive = TRUE, force = TRUE)
+  unlink(pptx_file, force = TRUE)
+})
+
