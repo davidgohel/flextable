1a9e22a0b6af288eb328ea15faef9f204ed63983
dgohel <david.gohel@ardata.fr>
2017-08-30 13:29:15 +0200

    allow bare name columns

--- R/lazy_display.R
+++ R/lazy_display.R
@@ -3,7 +3,7 @@
 lazy_format_simple <- function( col_key ){
 
   obj <- display_parser$new(x = paste0("{{", col_key, "}}"),
-                            formatters = list( as.formula( paste0(col_key, "~format(", col_key, ", justify = \"none\", trim = T)") ) ),
+                            formatters = list( as.formula( paste0(col_key, "~format(`", col_key, "`, justify = \"none\", trim = T)") ) ),
                             fprops = list() )
   obj
 }
