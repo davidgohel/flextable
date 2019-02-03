532da7c4bb709723c1ef21706596c4b9b2d6f263
David Gohel <david.gohel@ardata.fr>
2019-02-03 01:38:10 +0100

    as_flextable

--- R/00_generic.R
+++ R/00_generic.R
@@ -5,3 +5,14 @@ format_fun <- function( x, na_string = "", ... ){
   UseMethod("format_fun")
 }
 
+#' @title method to convert object to flextable
+#' @description This is a convenient function
+#' to let users create flextable bindings
+#' from any objects.
+#' @param x object to be transformed as flextable
+#' @param ... arguments for custom methods
+#' @export
+as_flextable <- function( x, ... ){
+  UseMethod("as_flextable")
+}
+
