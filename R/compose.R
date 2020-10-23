239e6cbb6db1bde29cf6bcaebe0dcfbd6aed9ecd
David Gohel <david.gohel@ardata.fr>
2020-10-23 22:53:06 +0200

    mv display.R to compose.R and footnote.R

---
+++ R/compose.R
@@ -0,0 +1,52 @@
+#' @export
+#' @importFrom rlang eval_tidy enquo
+#' @title Define flextable displayed values
+#' @description Modify flextable displayed values. Function is
+#' handling complex formatting as well as image insertion.
+#' @param x a flextable object
+#' @param i rows selection
+#' @param j column selection
+#' @param value a call to function \code{\link{as_paragraph}}.
+#' @param part partname of the table (one of 'all', 'body', 'header', 'footer')
+#' @examples
+#' library(officer)
+#' ft <- flextable(head( mtcars, n = 10))
+#' ft <- compose(ft, j = "carb", i = ~ drat > 3.5,
+#'   value = as_paragraph("carb is ", as_chunk( sprintf("%.1f", carb)) )
+#'   )
+#' ft <- autofit(ft)
+#' @export
+#' @family cells formatters
+#' @section Illustrations:
+#'
+#' \if{html}{\figure{fig_compose_1.png}{options: width=80\%}}
+compose <- function(x, i = NULL, j = NULL, value , part = "body"){
+
+  if( !inherits(x, "flextable") ) stop("compose supports only flextable objects.")
+  part <- match.arg(part, c("all", "body", "header", "footer"), several.ok = FALSE )
+
+  if( part == "all" ){
+    for( p in c("header", "body", "footer") ){
+      x <- compose(x = x, i = i, j = j, value = value, part = p)
+    }
+    return(x)
+  }
+
+  if( nrow_part(x, part) < 1 )
+    return(x)
+
+  check_formula_i_and_part(i, part)
+  i <- get_rows_id(x[[part]], i )
+  j <- get_columns_id(x[[part]], j )
+  x[[part]]$content[i, j] <- eval_tidy(enquo(value), data = x[[part]]$dataset[i,, drop = FALSE])
+
+  x
+}
+
+#' @rdname compose
+#' @section mk_par:
+#' Function \code{mk_par} is another name for \code{compose} as
+#' there is an unwanted conflict with package \code{purrr}.
+#' @export
+mk_par <- compose
+
--- R/footnote.R
+++ R/footnote.R
@@ -1,56 +1,4 @@
 #' @export
-#' @importFrom rlang eval_tidy enquo
-#' @title Define flextable displayed values
-#' @description Modify flextable displayed values. Function is
-#' handling complex formatting as well as image insertion.
-#' @param x a flextable object
-#' @param i rows selection
-#' @param j column selection
-#' @param value a call to function \code{\link{as_paragraph}}.
-#' @param part partname of the table (one of 'all', 'body', 'header', 'footer')
-#' @examples
-#' library(officer)
-#' ft <- flextable(head( mtcars, n = 10))
-#' ft <- compose(ft, j = "carb", i = ~ drat > 3.5,
-#'   value = as_paragraph("carb is ", as_chunk( sprintf("%.1f", carb)) )
-#'   )
-#' ft <- autofit(ft)
-#' @export
-#' @family cells formatters
-#' @section Illustrations:
-#'
-#' \if{html}{\figure{fig_compose_1.png}{options: width=80\%}}
-compose <- function(x, i = NULL, j = NULL, value , part = "body"){
-
-  if( !inherits(x, "flextable") ) stop("compose supports only flextable objects.")
-  part <- match.arg(part, c("all", "body", "header", "footer"), several.ok = FALSE )
-
-  if( part == "all" ){
-    for( p in c("header", "body", "footer") ){
-      x <- compose(x = x, i = i, j = j, value = value, part = p)
-    }
-    return(x)
-  }
-
-  if( nrow_part(x, part) < 1 )
-    return(x)
-
-  check_formula_i_and_part(i, part)
-  i <- get_rows_id(x[[part]], i )
-  j <- get_columns_id(x[[part]], j )
-  x[[part]]$content[i, j] <- eval_tidy(enquo(value), data = x[[part]]$dataset[i,, drop = FALSE])
-
-  x
-}
-
-#' @rdname compose
-#' @section mk_par:
-#' Function \code{mk_par} is another name for \code{compose} as
-#' there is an unwanted conflict with package \code{purrr}.
-#' @export
-mk_par <- compose
-
-#' @export
 #' @title add footnotes to flextable
 #' @description add footnotes to a flextable object. A symbol is appened
 #' where the footnote is defined and the note is appened in the footer part
@@ -102,7 +50,7 @@ mk_par <- compose
 #'
 #' \if{html}{\figure{fig_footnote_2.png}{options: width=70\%}}
 footnote <- function (x, i = NULL, j = NULL, value, ref_symbols = NULL, part = "body",
-          inline = FALSE, sep = "; ")
+                      inline = FALSE, sep = "; ")
 {
   if (!inherits(x, "flextable"))
     stop("footnote supports only flextable objects.")
@@ -149,7 +97,7 @@ footnote <- function (x, i = NULL, j = NULL, value, ref_symbols = NULL, part = "
 
     if(n_row > 0){
       new_inline <- list(x[["footer"]]$content[n_row, 1][[1]],
-             sep,new_inline[[1]])
+                         sep,new_inline[[1]])
       new_inline <- rbind.match.columns(new_inline)
       new_inline$seq_index <- seq_len(nrow(new_inline))
       new_inline <- list(new_inline)
