615a7cc7aac889b9fb1f662068bbb04d50aae606
Stefan Kloppenborg <stefan@kloppenborg.ca>
2020-12-01 17:39:18 -0500

    Added a test for footnote_multi_callout

--- tests/testthat/test-footnote.R
+++ tests/testthat/test-footnote.R
@@ -88,3 +88,37 @@ test_that("use formulas and character to target footnotes in body", {
     default_chunk_fmt = ft$body$styles$text )$txt
   expect_equal(expected_body, has_)
 })
+
+test_that("add footnote with callouts in several cells in body", {
+  ft <- flextable(head(iris))
+  ft <- footnote_multi_callout(ft,
+                               i_list = list(1, 3, ~ Petal.Length == 1.7),
+                               j_list = list(1, 2, "Petal.Length"),
+                               value = as_paragraph("This is footnote one"),
+                               ref_symbol = "(a)")
+  ft <- footnote_multi_callout(ft, i_list = list(1, 5), j_list = list(1, 2),
+                               value = as_paragraph("This is footnote two"),
+                               ref_symbol = "(b)")
+
+  # Check the footnotes themselves
+  expected_footnote <- c(
+    "(a)", "This is footnote one",
+    "(b)", "This is footnote two"
+  )
+  has_ <- flextable:::fortify_content(
+    ft$footer$content,
+    default_chunk_fmt = ft$footer$styles$text )$txt
+  expect_equal(expected_footnote, has_[1:4])
+
+  # Check that the footnotes callouts are in the right place
+  expected_body <- c("5.1", "(a)", "(b)", "4.9", "4.7", "4.6", "5.0",
+                     "5.4", "3.5", "3.0", "3.2", "(a)", "3.1", "3.6",
+                     "(b)", "3.9", "1.4", "1.4", "1.3", "1.5", "1.4",
+                     "1.7", "(a)", "0.2", "0.2", "0.2", "0.2", "0.2",
+                     "0.4", "setosa", "setosa", "setosa", "setosa",
+                     "setosa", "setosa")
+  has_ <- flextable:::fortify_content(
+    ft$body$content,
+    default_chunk_fmt = ft$body$styles$text)$txt
+  expect_equal(expected_body, has_)
+})
