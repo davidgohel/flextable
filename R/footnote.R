9362f108794b17049ce900970e6b6a36ceb26727
Stefan Kloppenborg <stefan@kloppenborg.ca>
2020-12-01 16:34:01 -0500

    Documentation for footnote_multi_callout

--- R/footnote.R
+++ R/footnote.R
@@ -49,6 +49,9 @@
 #' \if{html}{\figure{fig_footnote_1.png}{options: width=70\%}}
 #'
 #' \if{html}{\figure{fig_footnote_2.png}{options: width=70\%}}
+#'
+#' @seealso \code{\link{footnote_multi_callout}}
+#'
 footnote <- function (x, i = NULL, j = NULL, value, ref_symbols = NULL, part = "body",
                       inline = FALSE, sep = "; ")
 {
@@ -56,7 +59,7 @@ footnote <- function (x, i = NULL, j = NULL, value, ref_symbols = NULL, part = "
     stop("footnote supports only flextable objects.")
   part <- match.arg(part, c("body", "header", "footer"),
                     several.ok = FALSE)
-  if (part == "all") {  # TODO: What does this do? Does it need to be reproduced for the new function??
+  if (part == "all") {
     for (p in c("header", "body", "footer")) {
       x <- compose(x = x, i = i, j = j, value = value,
                    part = p)
@@ -79,39 +82,85 @@ footnote <- function (x, i = NULL, j = NULL, value, ref_symbols = NULL, part = "
   x
 }
 
-#' TODO: Document this function
-#' TODO: Maybe body should be a list??
 #' @export
-footnote_multi_callout <- function(x, i_list, j_list, value, ref_symbols = NULL,
+#' @title add a footnote with multiple callouts to a flextable
+#'
+#' @description add a footnotes to a flextable object. A symbol is appened
+#' in all the locations where the footnote is defined (the callout) and the
+#' note is appended in the footer part of the table.
+#' Unlike \code{\link{footnote}}, this function can only create one footnote,
+#' which may be referenced in several cells within the same part of the table.
+#'
+#' @param x a flextable object
+#' @param i_list a list of rows selections
+#' @param j_list a list of column selections
+#' @param value a call to function \code{\link{as_paragraph}}.
+#' @param ref_symbol character value. The symbol to append that will be used
+#' as references to notes.
+#' @param part partname of the table (one of 'body', 'header', 'footer')
+#' @param inline whether to add footnote on same line as previous footnote or not
+#' @param sep inline = T, character string to use as a separator between footnotes
+#' @examples
+#' ft1 <- flextable(head(iris))
+#' ft1 <- footnote_multi_callout(ft1,
+#'                              i_list = list(1, 6, 3),
+#'                              j_list = list(1, 2, 3),
+#'                              value = as_paragraph("This is one footnote"),
+#'                              ref_symbol = "(a)")
+#' ft1 <- valign(ft1, valign = "bottom", part = "header")
+#' autofit(ft1)
+#'
+#' ft2 <- flextable(head(iris))
+#' ft2 <- footnote_multi_callout(ft2,
+#'                              i_list = list(1, 3, ~ Petal.Length == 1.7),
+#'                              j_list = list(1, 2, "Petal.Length"),
+#'                              value = as_paragraph("This is footnote one"),
+#'                              ref_symbol = "(a)",
+#'                              inline = TRUE)
+#' ft2 <- footnote_multi_callout(ft2, i_list = list(1, 5), j_list = list(1, 2),
+#'                              value = as_paragraph("This is footnote two"),
+#'                              ref_symbol = "(b)",
+#'                              inline = TRUE)
+#' ft2 <- valign(ft2, valign = "bottom", part = "header")
+#' autofit(ft2)
+#'
+#' @section Illustrations:
+#'
+#' \if{html}{\figure{fig_footnote_multi_callout_1.png}{options: width=70\%}}
+#'
+#' \if{html}{\figure{fig_footnote_multi_callout_2.png}{options: width=70\%}}
+#'
+#' @seealso \code{\link{footnote}}
+#'
+footnote_multi_callout <- function(x, i_list, j_list, value, ref_symbol = NULL,
                               part = "body", inline = FALSE, sep = "; ") {
   if (!inherits(x, "flextable"))
     stop("footnote_multi_callout supports only flextable objects.")
   part <- match.arg(part, c("body", "header", "footer"),
                     several.ok = FALSE)
-  # TODO: do we need to do the part=="all" thing??
   if (nrow_part(x, part) < 1)
     return(x)
   if (length(value) > 1) {
     stop("footnote_multi_callout supports only a single value.")
   }
-  if (length(ref_symbols) > 1) {
+  if (length(ref_symbol) > 1) {
     stop("footnote_multi_callout supports only a single value of ref_symbols.")
   }
   if (length(i_list) != length(j_list)) {
     stop("i_list and j_list must be the same length.")
   }
 
-  if (is.null(ref_symbols)) {
-    ref_symbols <- as.character(1)
+  if (is.null(ref_symbol)) {
+    ref_symbol <- as.character(1)
   }
-  symbols_chunks <- list(as_sup(ref_symbols))
+  symbols_chunks <- list(as_sup(ref_symbol))
 
   for (indexi in seq_along(i_list)) {
     i <- i_list[[indexi]]
     j <- j_list[[indexi]]
     x <- footnote_callout(x, i, j, symbols_chunks, part)
   }
-  x <- footnote_value(x, value, ref_symbols, symbols_chunks, inline, sep)
+  x <- footnote_value(x, value, ref_symbol, symbols_chunks, inline, sep)
   x
 }
 
@@ -139,7 +188,7 @@ footnote_value <- function(x, value, ref_symbols, symbols_chunks, inline, sep) {
 
   if (inline){
     sep <- as_paragraph(sep)[[1]]
-    new[-1] <-lapply(new[-1], function(x) rbind.match.columns(list(sep, x)))
+    new[-1] <- lapply(new[-1], function(x) rbind.match.columns(list(sep, x)))
     new_inline <- list(rbind.match.columns(new))
     new_inline[[1]]$seq_index <- seq_len(nrow(new_inline[[1]]))
 
