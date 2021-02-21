167543192ecc0a633a9c52b3a8959483ba793c07
David Gohel <david.gohel@ardata.fr>
2021-02-21 22:59:21 +0100

    fix test

--- tests/testthat/test-captions.R
+++ tests/testthat/test-captions.R
@@ -15,21 +15,25 @@ rmd_str <- c("---", "output:", "  bookdown::word_document2:", "    keep_md: true
 rmd_file <- tempfile(fileext = ".Rmd")
 writeLines(rmd_str, rmd_file)
 
-test_that("row height is valid", {
+test_that("word_document2 captions", {
+  skip_if_not_installed("bookdown")
   render(rmd_file, output_file = "caption.docx", quiet = TRUE)
   dir_xml <- tempfile(pattern = "xml_tmp")
   officer::unpack_folder(file.path(dirname(rmd_file), "caption.docx"), dir_xml)
 
   xml_file <- file.path(dir_xml, "/word/document.xml" )
   x <- read_xml(xml_file)
-  caption1 <- xml_find_first(x, "//w:p[2]")
-  ref_1 <- list(pPr = list(pStyle = structure(list(), val = "TableCaption")),
-                r = list(t = structure(list("Table 1: a table caption"), space = "preserve")))
-  expect_equal(as_list(caption1), ref_1)
 
-  caption2 <- xml_find_first(x, "//w:p[4]")
-  ref_2 <- list(pPr = list(pStyle = structure(list(), val = "TableCaption")),
-                r = list(t = structure(list("Table 2: a table caption"), space = "preserve")))
-  expect_equal(as_list(caption2), ref_2)
+  caption <- xml_find_first(x, "//w:p[2]")
+  pstyle <- xml_child(caption, "w:pPr/w:pStyle")
+  expect_equal(xml_attr(pstyle, "val"), "TableCaption")
+  txt <- xml_child(caption, "w:r/w:t")
+  expect_equal(xml_text(txt), "Table 1: a table caption")
+
+  caption <- xml_find_first(x, "//w:p[4]")
+  pstyle <- xml_child(caption, "w:pPr/w:pStyle")
+  expect_equal(xml_attr(pstyle, "val"), "TableCaption")
+  txt <- xml_child(caption, "w:r/w:t")
+  expect_equal(xml_text(txt), "Table 2: a table caption")
 })
 
