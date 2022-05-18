433b8d0b38bc011bf42cba0dae7c30b478823979
David Gohel <david.gohel@ardata.fr>
2022-05-18 15:09:53 +0200

    feature: new function prepend_chunks

--- R/append_chunk.R
+++ R/append_chunk.R
@@ -92,3 +92,62 @@ append_chunks <- function (x, ..., i = NULL, j = NULL, part = "body"){
 
   x
 }
+#' @export
+#' @title prepend chunks to flextable content
+#' @description prepend chunks (for example chunk [as_chunk()])
+#' in a flextable.
+#' @param x a flextable object
+#' @param ... chunks to be prepended, see [as_chunk()], [gg_chunk()] and other
+#' chunk elements for paragraph.
+#' @param i rows selection
+#' @param j column selection
+#' @param part partname of the table (one of 'body', 'header', 'footer')
+#' @examples
+#' ft_1 <- flextable(head(iris))
+#' ft_1 <- prepend_chunks(
+#'   ft_1,
+#'   i = 1, j = 1,
+#'   colorize(as_b("coucou "), color = "red")
+#' )
+#' ft_1
+prepend_chunks <- function (x, ..., i = NULL, j = NULL, part = "body"){
+  if (!inherits(x, "flextable"))
+    stop("append_chunks supports only flextable objects.")
+  part <- match.arg(part, c("body", "header", "footer"),
+                    several.ok = FALSE)
+
+  if (nrow_part(x, part) < 1)
+    return(x)
+
+  check_formula_i_and_part(i, part)
+  i <- get_rows_id(x[[part]], i)
+  j <- get_columns_id(x[[part]], j)
+
+  col_exprs <- enquos(...)
+
+  tmp_data <- x[[part]]$dataset[i, , drop = FALSE]
+
+  for(col_expr in col_exprs){
+
+    value <- eval_tidy(col_expr, data = tmp_data)
+
+    if(is.data.frame(value) && nrow(value) == 1L && nrow(value) < nrow(tmp_data)){
+      value <- rep(list(value), nrow(tmp_data))
+      value <- rbind.match.columns(value)
+    }
+
+    if(is.data.frame(value))
+      value <- split(value, i)
+
+    new <- mapply(function(x, y) {
+      y$seq_index <- min(x$seq_index, na.rm = TRUE) - 1
+      z <- rbind.match.columns(list(y, x))
+      z$seq_index <- seq_len(nrow(z))
+      z
+    }, x = x[[part]]$content[i, j], y = value, SIMPLIFY = FALSE)
+
+    x[[part]]$content[i, j] <- new
+  }
+
+  x
+}
