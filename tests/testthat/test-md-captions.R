718508e0007a581f133ef447b3ae918dae557c53
David Gohel <david.gohel@ardata.fr>
2022-09-16 18:42:44 +0200

    tests: new visual tests

---
+++ tests/testthat/test-md-captions.R
@@ -0,0 +1,73 @@
+context("check markdown captions")
+
+library(rmarkdown)
+library(xml2)
+library(officer)
+
+skip_on_cran()
+skip_on_os("linux")
+skip_on_os("solaris")
+
+skip_if_not_installed("doconv")
+skip_if_not_installed("magick")
+skip_if_not_installed("webshot")
+skip_if_not(locatexec::exec_available("word"))
+skip_if_not(pandoc_version() >= numeric_version("2"))
+
+to_image_script <- "to-img.R"
+if (!file.exists(to_image_script)) {#just for dev purpose
+  to_image_script <- "tests/testthat/to-img.R"
+}
+source(to_image_script)
+
+
+init_flextable_defaults()
+
+get_output_file <- function(rmd){
+  basen <- tools::file_path_sans_ext(basename(rmd))
+  list.files(path = dirname(rmd), pattern = paste0(basen, ".(html|pdf|docx|pptx)$"),
+             full.names = TRUE)
+}
+
+test_that("rmarkdown caption", {
+  local_edition(3)
+
+  rmd_file <- tempfile(fileext = ".Rmd")
+  file.copy("rmd/rmarkdown.Rmd", rmd_file)
+  render(rmd_file, output_format = rmarkdown::pdf_document(latex_engine = "xelatex"),
+         envir = new.env(), quiet = TRUE)
+  expect_snapshot_rmd(name = "rmarkdown_pdf_document", get_output_file(rmd_file), format = "pdf")
+
+  rmd_file <- tempfile(fileext = ".Rmd")
+  file.copy("rmd/rmarkdown.Rmd", rmd_file)
+  render(rmd_file, output_format = "rmarkdown::word_document", envir = new.env(), quiet = TRUE)
+  expect_snapshot_rmd(name = "rmarkdown_word_document", get_output_file(rmd_file), format = "docx")
+
+  rmd_file <- tempfile(fileext = ".Rmd")
+  file.copy("rmd/rmarkdown.Rmd", rmd_file)
+  render(rmd_file, output_format = "rmarkdown::html_document", envir = new.env(), quiet = TRUE)
+  expect_snapshot_rmd(name = "rmarkdown_html_document", get_output_file(rmd_file), format = "html")
+})
+
+test_that("bookdown caption", {
+  local_edition(3)
+
+  rmd_file <- tempfile(fileext = ".Rmd")
+  file.copy("rmd/bookdown.Rmd", rmd_file)
+  render(rmd_file, output_format = bookdown::pdf_document2(latex_engine = "xelatex"),
+         envir = new.env(), quiet = TRUE)
+  expect_snapshot_rmd(name = "bookdown_pdf_document2", get_output_file(rmd_file), format = "pdf")
+
+  rmd_file <- tempfile(fileext = ".Rmd")
+  file.copy("rmd/bookdown.Rmd", rmd_file)
+  render(rmd_file, output_format = "bookdown::word_document2", envir = new.env(), quiet = TRUE)
+  expect_snapshot_rmd(name = "bookdown_word_document2", get_output_file(rmd_file), format = "docx")
+
+  rmd_file <- tempfile(fileext = ".Rmd")
+  file.copy("rmd/bookdown.Rmd", rmd_file)
+  render(rmd_file, output_format = "bookdown::html_document2", envir = new.env(), quiet = TRUE)
+  expect_snapshot_rmd(name = "bookdown_html_document2", get_output_file(rmd_file), format = "html")
+})
+
+
+init_flextable_defaults()
---
+++ tests/testthat/to-img.R
@@ -0,0 +1,103 @@
+compare_image <- function(img1, img2){
+  require("magick")
+  img.reference <- image_raster(magick::image_read(img1), tidy = FALSE)
+  img.empty <- image_raster(magick::image_read(img2), tidy = FALSE)
+  sum(img.reference == img.empty) == length(img.reference)
+}
+
+save_pptx <- function(x, path) {
+  require("doconv")
+  pptx_path <- save_as_pptx(x, path = tempfile(fileext = ".pptx"))
+  to_miniature(pptx_path, fileout = path, width = 1600)
+  path
+}
+
+save_docx <- function(x, path) {
+  require("doconv")
+  docx_path <- save_as_docx(x, path = tempfile(fileext = ".docx"))
+  to_miniature(docx_path, fileout = path, width = 1000)
+  path
+}
+save_html <- function(x, path) {
+  require("webshot")
+  html_file <- tempfile(fileext = ".html")
+  save_as_html(x, path = html_file)
+  curr_wd <- getwd()
+  path <- flextable:::absolute_path(path)
+  setwd(dirname(html_file))
+  tryCatch({
+    webshot::webshot(url = basename(html_file), file = path, zoom = 3, expand = 0 )
+  }, finally = {
+    setwd(curr_wd)
+  })
+  path
+}
+
+docx_to_miniature <- function(x, path) {
+  require("doconv")
+  to_miniature(x, fileout = path, width = 1000)
+  path
+}
+pdf_to_miniature <- function(x, path) {
+  require("doconv")
+  to_miniature(x, fileout = path, width = 1000)
+  path
+}
+pptx_to_miniature <- function(x, path) {
+  require("doconv")
+  to_miniature(x, fileout = path, width = 1200)
+  path
+}
+html_to_miniature <- function(x, path) {
+  curr_wd <- getwd()
+  x <- flextable:::absolute_path(x)
+  setwd(dirname(x))
+  tryCatch({
+    webshot::webshot(url = basename(x), file = path, zoom = 3, expand = 0 )
+  }, finally = {
+    setwd(curr_wd)
+  })
+  path
+}
+
+expect_snapshot_to <- function(name, x, format = "docx") {
+  require("doconv")
+  require("magick")
+  stopifnot(locatexec::exec_available("word"))
+
+  name <- paste0(name, ".png")
+  announce_snapshot_file(name = name)
+  if ("docx" %in% format) {
+    path <- save_docx(x, path = tempfile(fileext = ".png"))
+  } else if ("pptx" %in% format) {
+    path <- save_pptx(x, path = tempfile(fileext = ".png"))
+  } else if ("pdf" %in% format) {
+    path <- save_pdf(x, path = tempfile(fileext = ".png"))
+  } else if ("html" %in% format) {
+    path <- save_html(x, path = tempfile(fileext = ".png"))
+  } else {
+    path <- save_docx(x, path = tempfile(fileext = ".png"))
+  }
+  expect_snapshot_file(path, name, compare = compare_image)
+}
+
+expect_snapshot_rmd <- function(name, x, format = "docx") {
+  require("doconv")
+  require("magick")
+
+  name <- paste0(name, ".png")
+  announce_snapshot_file(name = name)
+  if ("docx" %in% format) {
+    path <- docx_to_miniature(x, path = tempfile(fileext = ".png"))
+  } else if ("pptx" %in% format) {
+    path <- pptx_to_miniature(x, path = tempfile(fileext = ".png"))
+  } else if ("pdf" %in% format) {
+    path <- pdf_to_miniature(x, path = tempfile(fileext = ".png"))
+  } else if ("html" %in% format) {
+    path <- html_to_miniature(x, path = tempfile(fileext = ".png"))
+  } else {
+    stop("cas non gere")
+  }
+  expect_snapshot_file(path, name, compare = compare_image)
+}
+
