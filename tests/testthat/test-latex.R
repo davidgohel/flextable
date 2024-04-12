7ac8b14b8c0329e37ab3dbb0ec31f4e09cc9839a
David Gohel <david.gohel@ardata.fr>
2024-04-12 20:12:26 +0200

    tests: fix test by setting quarto.version as numeric_version

--- tests/testthat/test-latex.R
+++ tests/testthat/test-latex.R
@@ -15,17 +15,17 @@ test_that("fonts are defined in latex", {
     flextable::font(fontname = "Liberation Sans", part = "body")
 
   # R Markdown with pdflatex
-  knitr::opts_knit$set("quarto.version"=NULL)
+  knitr::opts_knit$set("quarto.version" = NULL)
   latex_str <- flextable:::gen_raw_latex(ft, quarto = FALSE)
   expect_no_match(latex_str, regexp = "Liberation Sans", fixed = TRUE)
 
-  knitr::opts_knit$set("rmarkdown.pandoc.args"= c("--pdf-engine", "xelatex"))
+  knitr::opts_knit$set("rmarkdown.pandoc.args" = c("--pdf-engine", "xelatex"))
   latex_str <- flextable:::gen_raw_latex(ft, quarto = FALSE)
   expect_match(latex_str, regexp = "Liberation Sans", fixed = TRUE)
-  knitr::opts_knit$set("rmarkdown.pandoc.args"= NULL)
+  knitr::opts_knit$set("rmarkdown.pandoc.args" = NULL)
 
   # quarto
-  knitr::opts_knit$set("quarto.version"=1)
+  knitr::opts_knit$set("quarto.version" = numeric_version(1))
   latex_str <- flextable:::gen_raw_latex(ft, quarto = TRUE)
   expect_match(latex_str, regexp = "Liberation Sans", fixed = TRUE)
 })
